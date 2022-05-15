0b0bb35443b5f974e03d5039dd94bd9be819fedb
misostack <misostack.com@gmail.com>
2022-05-15 09:25:23 +0700

    Update debug

---
+++ .mocharc.local.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// This is a JavaScript-based config file containing every Mocha option plus others.
+// If you need conditional logic, you might want to use this type of config,
+// e.g. set options via environment variables 'process.env'.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  recursive: true,
+  retries: 1,
+  slow: "75",
+  sort: false,
+  spec: ["test/*.spec.js", "test/**/*.spec.js", "test/**/**/*.spec.js"], // the positional arguments!
+};
---
+++ .mocharc.single.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// This is a JavaScript-based config file containing every Mocha option plus others.
+// If you need conditional logic, you might want to use this type of config,
+// e.g. set options via environment variables 'process.env'.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  retries: 1,
+  slow: "75",
+  sort: false,
+  timeout: false,
+};
